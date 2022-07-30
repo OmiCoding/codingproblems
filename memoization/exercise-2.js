9815ab853937df5a5a8e00126b45a9795976c9b2
Omi <digicoder@protonmail.com>
2022-07-30 19:53:27 -0400

    made some changes to one of the files

--- memoization/exercise-2.js
+++ memoization/exercise-2.js
@@ -3,13 +3,17 @@ const times10 = (n) => n * 10;
 // Task 3: Clean up your global scope by moving your cache inside your function.
 // protip: Use a closure to return a function that you can call later.
 
+// TC: O(1)
+// SC: O(n)
+
 const memoizedClosureTimes10 = () => {
-  let cache = {};
+  let cache = {}; // SC: O(n)
   return (n) => {
     if (cache[n]) {
+      // TC: O(1)
       return cache[n];
     }
-    cache[n] = n * 10;
+    cache[n] = n * 10; // TC: O(1)
     return cache[n];
   };
 };
