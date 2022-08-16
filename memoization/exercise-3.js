49704067ab6cd76e96b1de78c5da40207a80c40f
Omi <digicoder@protonmail.com>
2022-08-16 16:05:56 -0400

    new commit

--- memoization/exercise-3.js
+++ memoization/exercise-3.js
@@ -1,10 +1,22 @@
-const times10 = (n) => n * 10;
+const times10 = function (n) {
+  return n * 10;
+};
 // Task 4: Make your memo function generic and accept the times10 function as a callback rather than defining the n * 10 logic inside the if/else or pulling it in from the global scope.
 
 // protip: Take advantage of the fact that parameters are saved in the closure as well, just like the cache from the previous example.
-const memoize = (cb) => {
-  return cb;
-};
+function memoize(cb) {
+  let cache = {};
+  return function (n) {
+    if (n in cache) {
+      console.log(cache);
+      return cache[n];
+    } else {
+      let result = cb(n);
+      cache[n] = result;
+      return result;
+    }
+  };
+}
 
 // returned function from memoizedAdd
 const memoizedTimes10 = memoize(times10);
---
+++ memoization/exercise-4.js
@@ -0,0 +1,28 @@
+function factorial(y, sum = 1) {
+  if (y === 0) {
+    return sum;
+  }
+  sum *= y;
+  return factorial(y - 1, sum);
+}
+
+function memoize(cb) {
+  let cache = {};
+  return function (n) {
+    console.log(cache);
+
+    if (n in cache) {
+      return cache[n];
+    } else {
+      let result = cb(n);
+      cache[n] = result;
+      return result;
+    }
+  };
+}
+
+let memoizeFactorial = memoize(factorial);
+let memoizedVal = memoizeFactorial(10);
+let memoizedVal2 = memoizeFactorial(2);
+console.log("Factorial 10 :", memoizedVal);
+console.log("Cached factorial 10!", memoizedVal2);
